871e0118f88dc9fe19af57e90d1e49ac0b240165
CCY <amy914302@gmail.com>
2026-03-12 19:57:42 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,5 @@
 // js/config.js
 const SUPABASE_URL = 'https://xhvlwmcrgwskaforgxqd.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_nluuQ4-miY-nunLCYrvTrA_z577O8Pc';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhodmx3bWNyZ3dza2Fmb3JneHFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxMTc3NjMsImV4cCI6MjA4ODY5Mzc2M30.kpAvMhfEXF-HsUUxi7awuYyx0u1As3mqdwusBcpEKvo';
 
-// 🌟 重點：我們用 window.supabaseClient 讓它變成全域通用的工具
 window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
